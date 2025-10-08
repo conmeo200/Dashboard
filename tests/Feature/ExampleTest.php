5af3816705395563dd4bfd30e41c64f1a797b19e
Anh Tuấn Trần <anhtuantran356@gmail.com>
2025-10-08 15:40:28 -0700

    test CI

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,8 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $result = 2 + 3;
 
-        $response->assertStatus(200);
+        $this->assertEquals(5, $result);
     }
 }
